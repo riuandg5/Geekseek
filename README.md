41ed4caa260f80ff5123ec1a11af1af34d03432c
sachin <iamsainisachin@gmail.com>
2017-12-30 21:32:41 +0530

    add styling and js fles

---
+++ README.md
@@ -0,0 +1,8 @@
+# GEEKSEEK
+A web project to provide students with study material.
+
+# TCHNOLOGIES USED
+* Node.js
+* Express
+* Mongodb
+* HTML CSS (obviously)
---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+
+var port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+
+app.get("/",function(req,res){
+    res.render("index");
+})
+
+app.listen(port,function(){
+    console.log('server started');
+});
