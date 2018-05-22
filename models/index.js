771930bea6f3ed0ec6d01f22af6a920742cabc7e
Sachin Saini <iamsainisachin@gmail.com>
2018-05-22 19:09:48 +0530

    add db env vars

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose'); // require mongoose
-mongoose.set('debug',true); // mongoose debug mode true for testing purposes remove when deployinng
+mongoose.set('debug', true); // mongoose debug mode true for testing purposes remove when deployinng
 mongoose.Promise = Promise; // use mongoose promise library to use promises instead of nasty callbacks
-mongoose.connect('mongodb://localhost/geek'); // connect to data base use more secure way to hide db info in future
+const dburl = process.env.DBURL || "mongodb://localhost/geek";
+
+mongoose.connect(dburl);
 // 'post' has our model and schema
 module.exports.post = require('./postmodel');
 module.exports.user = require('./usermodel');
