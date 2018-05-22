2aa05e8683923f97fc00ead1c9f0bb6f7340c323
Sachin Saini <iamsainisachin@gmail.com>
2018-05-22 19:10:41 +0530

    general changes related to authentication

--- routes/main.js
+++ routes/main.js
@@ -3,78 +3,30 @@ const router = express.Router();
 var db = require('../models');
 
 // root route
-router.get('/',function(req,res){
+router.get('/', function (req, res) {
     res.render('index');
 });
 
-// route to admin panel
-router.get("/gsad/admin",function(req,res){
-    res.render('admin-panel');
-});
 
 // route to admin upload panel
-router.get("/gsad/admin/upload",function(req,res){
-    res.render('admin-upload');
-});
 
-// route to admin view panel
-router.get("/gsad/admin/view",function(req,res){
-    db.post.find().sort({date: -1})
-    .then(function(allposts){
-        res.render('admin-view',{'allposts':allposts});
-    })
-    .catch(function(err){
-        res.send(err);
-    });
-});
-
-// route to admin delete panel
-router.get("/gsad/admin/delete",function(req,res){
-    db.post.find().sort({date: -1})
-    .then(function(allposts){
-        res.render('admin-delete',{'allposts':allposts});
-    })
-    .catch(function(err){
-        res.send(err);
-    });
-});
-
-// route to admin confirm deletion panel
-router.get('/gsad/admin/delete/:id',function(req,res){
-    db.post.findById(req.params.id)
-    .then(function(post){
-        res.render('admin-confirm-delete',{'post':post});
-    });
-});
 
 // auth routes
 
-router.get('/gsad/admin/register',function(req,res){
+router.get('/gsad/admin/register', function (req, res) {
     res.render('admin-register');
 });
 
-// handle signup logic
-router.post('/gsad/admin/register',function(req,res){
-    var newuser = new db.user({username: req.body.username});
-    db.user.register(newuser, req.body.password,function(err,user){
-        if(err){
-            console.log(err);
-            return res.render('/gsad/admin/register');
-        }
-        passport.authenticate('local')(req,res,function(){
-            res.redirect('/gsad/admin');
-        });
-    });
-});
+
 // route for different content types
-router.get('/:content',function(req,res){
-    db.post.find({'ctype': req.params.content}).sort({date: -1})
-    .then(function(cposts){
-        res.render('content-view',{'cposts':cposts});
-    })
-    .catch(function(err){
-        res.send(err);
-    });
+router.get('/:content', function (req, res) {
+    db.post.find({ 'ctype': req.params.content }).sort({ date: -1 })
+        .then(function (cposts) {
+            res.render('content-view', { 'cposts': cposts });
+        })
+        .catch(function (err) {
+            res.send(err);
+        });
 });
 
 // export the router to be used in app.js
