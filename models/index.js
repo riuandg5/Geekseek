334b40d1adebce7fab8ee79373cff7e48c775f19
riuandg5 <riuandg5@gmail.com>
2018-06-25 15:57:26 +0530

    Geekseek redefining

--- models/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose'); // require mongoose
-mongoose.set('debug', true); // mongoose debug mode true for testing purposes remove when deployinng
-mongoose.Promise = Promise; // use mongoose promise library to use promises instead of nasty callbacks
-const dburl = process.env.DBURL || "mongodb://localhost/geek";
-
-mongoose.connect(dburl);
-// 'post' has our model and schema
-module.exports.post = require('./postmodel');
-module.exports.user = require('./usermodel');
