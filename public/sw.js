178166d8aa0d75c869d1cce88a883168ccf08cf9
riuandg5 <riuandg5@gmail.com>
2018-07-01 13:25:32 +0530

    fetch from network only

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,12 @@
-self.addEventListener('install', function onServiceWorkerInstall(event){
-    console.log('Install event: ', event);
+// fetch resourceonly from network
+self.addEventListener('fetch', function onServiceWorkerFetch(event){
+    console.log('Fetch event: ', event.request.url);
+    event.respondWith(
+        fetch(event.request).then(function returnNetworkResponse(networkResponse){
+            console.log(`Fetch from network for ${event.request.url} is successfull.`);
+            return networkResponse
+        }).catch(function return(err){
+            console.log(`Fetch from network for ${event.request.url} is unsuccessfull: `, err);
+        })
+    );
 });
