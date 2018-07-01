a401eec3fe7e7e69a1c506e9a4c4cc4b676708e7
riuandg5 <riuandg5@gmail.com>
2018-07-01 13:37:22 +0530

    install event

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,7 @@
-// fetch resourceonly from network
+self.addEventListener('install', function onServiceWorkerInstall(event){
+    console.log('Install event: ', event);
+});
+// fetch resource only from network
 self.addEventListener('fetch', function onServiceWorkerFetch(event){
     console.log('Fetch event: ', event.request.url);
     event.respondWith(
