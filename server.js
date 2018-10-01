09c0931098f9fd76e894656bab80f0059c3c71d3
Sachin Saini <iamsainisachin@gmail.com>
2018-10-01 23:11:33 +0530

    use const instead of var

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-var port  = process.env.PORT || 3079,
+const port = process.env.PORT || 3079,
     dburi = process.env.DBURI || "mongodb://localhost/geekseek";
 // require npm packages
-var bodyParser       = require("body-parser"),
-    methodOverride   = require("method-override"),
+const bodyParser = require("body-parser"),
+    methodOverride = require("method-override"),
     expressSanitizer = require("express-sanitizer"),
-    mongoose         = require("mongoose"),
-    express          = require("express"),
-    flash            = require("connect-flash"),
-    passport		 = require("passport"),
-    LocalStrategy	 = require("passport-local"),
-    app              = express();
+    mongoose = require("mongoose"),
+    express = require("express"),
+    flash = require("connect-flash"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    app = express();
 // require models
-var Content          = require("./app/models/Content.model"),
-    Post 			 = require("./app/models/Post.model"),
-	User             = require("./app/models/User.model");
+const Content = require("./app/models/Content.model"),
+    Post = require("./app/models/Post.model"),
+    User = require("./app/models/User.model");
 // require routes
-var	contentsRoute    = require("./app/routes/contents.route"),
-    postsRoute       = require("./app/routes/posts.route"),
-	indexRoute       = require("./app/routes/index.route");
+const contentsRoute = require("./app/routes/contents.route"),
+    postsRoute = require("./app/routes/posts.route"),
+    indexRoute = require("./app/routes/index.route");
 // tell mongoose to use bluebird or native ES6 promise library
 // mongoose.Promise = global.Promise; // native ES6 promise library
 mongoose.Promise = require('bluebird');
@@ -31,10 +31,10 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 // passport configuration
 app.use(require("express-session")({
-	secret: "geekseekzlob",
-    cookie: {maxAge: 86400000},
-	resave: false,
-	saveUninitialized: false
+    secret: "geekseekzlob",
+    cookie: { maxAge: 86400000 },
+    resave: false,
+    saveUninitialized: false
 }));
 app.use(flash()); // use flash for flash messages
 app.use(passport.initialize());
@@ -43,17 +43,17 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 // middleware to send variables to every template page
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     // set currentUser to req.user and get superadmin id
-	res.locals.currentUser = req.user;
-    res.locals.superAdmin  = process.env.SUPERADMIN || '5b1d6a77e267e411d4cce1a6';
+    res.locals.currentUser = req.user;
+    res.locals.superAdmin = process.env.SUPERADMIN || '5b1d6a77e267e411d4cce1a6';
     // success and error message variables for flash
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-	next();
+    next();
 });
 // use body-parser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 // use express-sanitizer to sanitize request credentials
 app.use(expressSanitizer());
 // method-override to manipulate delete request
@@ -63,6 +63,6 @@ app.use(contentsRoute);
 app.use(postsRoute);
 app.use(indexRoute);
 // listen to the port
-app.listen(port, function(){
+app.listen(port, function () {
     console.log('Geekseek server started...');
 });
